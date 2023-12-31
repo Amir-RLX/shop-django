12f5347f568a4d32e8162e24b8c9bef83104d740
Amir-RLX <amir.rahati@gmail.com>
2023-12-31 09:38:59 +0330

    Venv bug

--- anisastore/store/models.py
+++ anisastore/store/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 import uuid
+from django.utils import timezone
+import os
+from django.core.validators import ValidationError
+from django.core.validators import FileExtensionValidator
 
 
 # Create your models here.
+
+def _get_file_name(obj, file):
+    name = uuid.uuid4()
+    ext = os.path.splitext(file)[1].lower()
+    path = timezone.now().strftime('product_image/%Y/%m/%d/')
+    return os.path.join(path, f'{name}{ext}')
+
+
+def image_dims_validator(image):
+    if image.height > 500 or image.width > 500:
+        raise ValidationError("size must be less than 500*500")
 
 
 class Product(models.Model):
@@ -17,10 +32,21 @@ class Product(models.Model):
     category = models.ForeignKey('Category',
                                  on_delete=models.PROTECT,
                                  related_name='products')
-    image = models.ImageField(upload_to='product_image/%Y/%m/%d')
+    image = models.ImageField(upload_to=_get_file_name, null=True, blank=True,
+                              validators=[FileExtensionValidator(['jpg', 'png']),
+                                          image_dims_validator])
+
+    # def clean_image(self):
+    #     if self.image.height > 500 or self.image.width > 500:
+    #         raise ValidationError("size must be less than 500*500")
+    #     return self.image
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        ...
+        return super().save(*args, **kwargs)
 
 
 class Category(models.Model):
